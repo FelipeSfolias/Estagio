1388293f95a9354b382bcafe00301b24301ef329
FelipeSfolias <141787514+FelipeSfolias@users.noreply.github.com>
2026-03-31 15:40:12 -0300

    Delete estagio directory

--- estagio/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("apps.accounts.urls")),
-    path("api/", include("apps.core.api_urls")),
-    path("", include("apps.core.urls")),
-]
